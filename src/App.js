d9a92deb9018b5b64b1f2148a90ba0f4c28ca1c3
MarianaAya <umemuramariana@gmail.com>
2022-01-20 09:29:16 -0300

    cpf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
-import ContadorDeCliques from './Components/ContadorDeCliques';
-import React from 'react'
+import React,{useState} from 'react';
+
+function MyInput({value,onChange}){
+  return <input value={value} onChange={ev=>onChange(ev.target.value)}/>
+}
+function NumericInput({component,value,onChange, ...inputProps}){
+  const numericValue = value.replace(/[^\d]/g,'');
+  const onChangeNumeric = value => onChange(value.replace(/[^\d]/g,''));
+  const InnerComponent = component;
+
+  return <InnerComponent value={numericValue} onChange={onChangeNumeric} {...inputProps}/>
+  
+}
+function formatarCPF(value){
+
+  if(value.length<14){
+    let cpf = value.replace(/[^\d]/g,'');
+  
+
+    cpf=cpf.replace(/(\d{3})(\d)/,'$1.$2');
+    cpf=cpf.replace(/(\d{3})(\d)/,'$1.$2');
+    cpf=cpf.replace(/(\d{3})(\d)/,'$1-$2');
+
+    return cpf;
+  }
+  else
+    return value.substring(0,14)
+
+  
+}
+function CPFInput({component,value,onChange,...inputProps}){
+  const InnerComponent = component;
+  
+  const valueCpf = formatarCPF(value)
+    
+  const onChangeCPF = value=>onChange(formatarCPF(value) )
+
+ 
+  
+  return <InnerComponent value={valueCpf} onChange={onChangeCPF} {...inputProps}/>
+}
 function App() {
-  const [quantidade,setQuantidade] = React.useState(0);
+  const [numero,setNumero] = useState("999");
+  const [cpf,setCpf] = useState("");
+  //const [date,setDate] = useState(new Date(2020,11,01));
   return (
     <div className="App">
-      <h2>Welcome to React</h2>
-      <ContadorDeCliques
-        textoDescritivo="Quantidade de cliques acima"
-        textoBotao="Botão superior"
-        quantidade={quantidade}
-        setQuantidade={setQuantidade}
-      />
-      <ContadorDeCliques
-        textoDescritivo="Quantidade de cliques abaixo"
-        textoBotao="Botão inferior"
-        quantidade={quantidade}
-        setQuantidade={setQuantidade}
-      />
+     
+        <NumericInput component={MyInput} value={numero} onChange={value=>setNumero(value)}/>
+        <CPFInput component={MyInput} value={cpf} onChange={value=>setCpf(value)}/>
+        <h2>Número: {numero}</h2>
+        <h2>CPF: {cpf}</h2>
     </div>
   );
 }
